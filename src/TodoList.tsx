30a7a37f3ae5e2a84643cf319db082058757ade6
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2022-02-03 18:11:18 +0900

    add: useForm practice

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const value = event.currentTarget.value;
-    setToDo(value);
+  const { register, handleSubmit, formState } = useForm();
+
+  const onValid = (data: any) => {
+    console.log(data);
   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(formState.errors);
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input {...register("Email", { required: true })} placeholder="Email" />
         <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="Write a to do"
-        ></input>
+          {...register("FirstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("LastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("Password", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short!",
+            },
+          })}
+          placeholder="Password"
+        />
         <button>Add</button>
       </form>
     </div>
