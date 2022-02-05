b85b3ee09674a965b3ca7977fe3770c2733a10bd
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2022-02-05 19:12:40 +0900

    add: setError

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,18 +1,53 @@
 import { useForm } from "react-hook-form";
 
-function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
+interface IForm {
+  Email: string;
+  FirstName: string;
+  LastName: string;
+  Password: string;
+  Password1: string;
+  extraError?: string;
+}
 
-  const onValid = (data: any) => {
+function ToDoList() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      Email: "@naver.com",
+    },
+  });
+
+  const onValid = (data: IForm) => {
+    if (data.Password !== data.Password1) {
+      setError(
+        "Password1",
+        { message: "Passwords are not the same." },
+        { shouldFocus: true }
+      );
+    }
     console.log(data);
   };
 
-  console.log(formState.errors);
+  console.log(errors);
 
   return (
     <div>
       <form onSubmit={handleSubmit(onValid)}>
-        <input {...register("Email", { required: true })} placeholder="Email" />
+        <input
+          {...register("Email", {
+            required: "Email required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only 'naver.com' emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.Email?.message}</span>
         <input
           {...register("FirstName", { required: true })}
           placeholder="First Name"
@@ -31,7 +66,19 @@ function ToDoList() {
           })}
           placeholder="Password"
         />
+        <input
+          {...register("Password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short!",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors.Password1?.message}</span>
         <button>Add</button>
+        <span>{errors.extraError?.message}</span>
       </form>
     </div>
   );
